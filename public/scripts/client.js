6ac061f7410d1e6d3a952758762e97dab59b082e
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-05-19 21:16:28 -0600

    added sliding error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,48 @@
 $(document).ready(function() {
   //sets timeago up to display the date posted below tweets
   const x = document.getElementsByClassName('need_to_be_rendered');
+  $('#error-message').hide();
+  loadTweets(renderAllTweets);
 
   $('form').on('submit', function(event){
     event.preventDefault();
     const data = $(this).serialize();
     const text = decodeURIComponent(data.substring(5));//decoded the urlencoded string
+    
     if (text.length === 0) {
-      alert('Your tweet must contain text!');
+      const errMessage = 'Tweets cannot be blank.'
+      $('#error-message').hide();
+      $('#error-message').empty();
+      $('#error-message').append(createError(errMessage));
+      $("#error-message").slideDown("slow");
+      $('textarea').focus();
     } else if (140 < text.length) {
-      alert('Your tweet cannot exceed 140 characters!');
+      const errMessage = 'Tweets cannot exceed 140 characters.'
+      $('#error-message').hide();
+      $('#error-message').empty();
+      $('#error-message').append(createError(errMessage));
+      $("#error-message").slideDown("slow");
+      $('textarea').focus();
     } else {
+      $('#error-message').hide();
       $.post('/tweets/', data, function() {
-        loadTweets();
+        loadTweets(renderLatestTweets);
+        $("#tweet-text").val("");
       });
+      $('textarea').focus();
     }
     
   })
-  // Test / driver code (temporary)
-
-  renderTweets(data);
-
-
 });
 
 const createTweetElement = (tweet) => {
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  const safeInput = escape(tweet.content.text);
+  
   const $newTweet = 
   `
   <article class="tweet">
@@ -48,7 +66,7 @@ const createTweetElement = (tweet) => {
           </div>
         </header>
         <div class="posted-tweet">
-          ${tweet.content.text}
+          ${safeInput}
         </div>
         <footer>
           <div class="date-posted need_to_be_rendered">${$.timeago(tweet.created_at)}</div>
@@ -63,38 +81,24 @@ const createTweetElement = (tweet) => {
   return $newTweet;
 };
 
-const renderTweets = (tweetArr) => {
+const createError = errMessage => {
+  const $err = (`<div class="alert-box error"><i class="fas fa-times-circle"></i><span>error: </span>${errMessage}</div>`);
+  return $err;
+};
+
+const renderAllTweets = (tweetArr) => {
   for (const item of tweetArr) {
     $('#tweet-container').prepend(createTweetElement(item));
   }
 };
 
-const loadTweets = () => {
-  $.get('/tweets/', renderTweets);
+const renderLatestTweets = tweetArr => {
+  const latest = tweetArr[tweetArr.length - 1];
+  $('#tweet-container').prepend(createTweetElement(latest));
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 2161116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+const loadTweets = (whichToRender) => {
+  $.get('/tweets/', whichToRender);
+};
+
 
